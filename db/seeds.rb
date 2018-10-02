56d142a84829a103494e0b591691e1bd5cf60338
Chris Powell <chris.ian.powell@gmail.com>
2018-10-02 08:08:32 -0600

    adds foods seeds to seeds.rb and adds serializer for food JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Food.create(name: 'Twinky', calories: 150)
+Food.create(name: 'Beer', calories: 154)
+Food.create(name: 'Oyster', calories: 175)
