88f4cc2701440391dca4c9da760b5c98f773c7e3
Chris Powell <chris.ian.powell@gmail.com>
2018-10-03 07:59:21 -0600

    adds almost complete post for food

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -2,4 +2,16 @@ class Api::V1::FoodsController < ApplicationController
   def index
     render json: Food.all
   end
+
+  def create
+    food = Food.create(food_params)
+    binding.pry
+    render json: food
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :calories)
+  end
 end
--- spec/requests/api/v1/food_post_spec.rb
+++ spec/requests/api/v1/food_post_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 describe 'post to create a new food' do
   context 'POST /api/v1/foods' do
     it 'can create new food' do
+      food = create(:food, name: 'twizlers', calories: 1)
 
+      post '/api/v1/foods', params: { 'food': { 'name': "#{food.name}", 'calories': "#{food.calories}"} }
+
+      expect(response).to be_successful
+
+      food_json = JSON.parse(response.body)
     end
   end
 end
