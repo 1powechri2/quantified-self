ab8ac5713433fe311fe1ceda5f8826b49ebf773e
Chris Powell <chris.ian.powell@gmail.com>
2018-10-06 14:14:24 -0600

    adds enpoint for individual meal

--- spec/requests/api/v1/meals_request_spec.rb
+++ spec/requests/api/v1/meals_request_spec.rb
@@ -24,4 +24,25 @@ describe 'the meals index page' do
       expect(meals_json[1][:foods].count).to eq(1)
     end
   end
+  context 'api/v1/meals' do
+    it 'returns a collection of meals and their individual collections of foods' do
+      meal1 = create(:meal)
+      meal2 = create(:meal)
+      food1 = create(:food, name: 'mud pie')
+      food2 = create(:food, name: 'blood pie')
+
+      create(:meal_food, meal_id: meal1.id, food_id: food1.id)
+      create(:meal_food, meal_id: meal1.id, food_id: food2.id)
+      create(:meal_food, meal_id: meal2.id, food_id: food2.id)
+
+      get "/api/v1/meals/#{meal1.id}/foods"
+
+      expect(response).to be_successful
+
+      meals_json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(meals_json[:name]).to eq(meal1.name)
+      expect(meals_json[:foods].count).to eq(2)
+    end
+  end
 end
